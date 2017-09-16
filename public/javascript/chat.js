b864589aaaa25804b71f8f48e723ecfd2637f430
Your Name <pooja232shetty@gmail.com>
2017-09-16 11:44:21 +0000

    hi

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -8,7 +8,7 @@ var app = angular.module('chatApp',['ngMaterial']);
 	};
 	$scope.trust = $sce.trustAsHtml;
 
-	var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket"); 
+	var  exampleSocket =  new  WebSocket("wss://swiftcode-pooja.herokuapp.com/chatSocket"); 
 	exampleSocket.onmessage  =   function  (event) {  
 		var jsonData = JSON.parse(event.data);  
 		jsonData.time = new Date().toLocaleTimeString(); 
